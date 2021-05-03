3a4bb87879742d20f036c5166a9f202516000db2
Mogly0815 <>
2021-05-03 21:25:28 +0000

    updated

--- main.py
+++ main.py
@@ -6,34 +6,35 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
+combined_name = name1 + name2
 
-T_counter = name1.lower().count("t")
-T_counter += name2.lower().count("t")
+T_counter = combined_name.lower().count("t")
+# T_counter += name2.lower().count("t")
 # print(f"T occurs {T_counter} times")
-R_counter = name1.lower().count("r")
-R_counter += name2.lower().count("r")
+R_counter = combined_name.lower().count("r")
+# R_counter += name2.lower().count("r")
 # print(f"R occurs {R_counter} times")
-U_counter = name1.lower().count("u")
-U_counter += name2.lower().count("u")
+U_counter = combined_name.lower().count("u")
+# U_counter += name2.lower().count("u")
 # print(f"U occurs {U_counter} times")
-E_counter = name1.lower().count("e")
-E_counter += name2.lower().count("e")
+E_counter = combined_name.lower().count("e")
+# E_counter += name2.lower().count("e")
 # print(f"E occurs {E_counter} times")
 
 
 
 
-L_counter = name1.lower().count("l")
-L_counter += name2.lower().count("l")
+L_counter = combined_name.lower().count("l")
+# L_counter += name2.lower().count("l")
 # print(f"L occurs {L_counter} times")
-O_counter = name1.lower().count("o")
-O_counter += name2.lower().count("o")
+O_counter = combined_name.lower().count("o")
+# O_counter += name2.lower().count("o")
 # print(f"O occurs {O_counter} times")
-V_counter = name1.lower().count("v")
-V_counter += name2.lower().count("v")
+V_counter = combined_name.lower().count("v")
+# V_counter += name2.lower().count("v")
 # print(f"V occurs {V_counter} times")
-E_counter = name1.lower().count("e")
-E_counter += name2.lower().count("e")
+E_counter = combined_name.lower().count("e")
+# E_counter += name2.lower().count("e")
 # print(f"E occurs {E_counter} times")
 
 true_sum = T_counter + R_counter + U_counter + E_counter
@@ -42,13 +43,12 @@ love_sum = L_counter + O_counter + V_counter + E_counter
 # print(f"Love-Buchstaben sind {love_sum}")
 
 
-sumup = str(true_sum) + str(love_sum)
-sumup = int(sumup)
+sumup = int(str(true_sum) + str(love_sum))
 
 
-if sumup < 10 or sumup > 90:
+if (sumup < 10) or (sumup > 90):
   print(f"Your score is {sumup}, you go together like coke and mentos.")
-elif sumup >= 40 and sumup <= 50:
+elif (sumup >= 40) and (sumup <= 50):
   print(f"Your score is {sumup}, you are alright together.")
 else:
   print(f"Your score is {sumup}.")
